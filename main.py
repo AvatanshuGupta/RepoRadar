22108b7fe968edda31d2ba790ad5bd0bf830470a
AvatanshuGupta <avatanshugupta@gmail.com>
2025-10-24 22:59:40 +0530

    basic functions added

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from reporadar!")
+from fastmcp import FastMCP
+import os
+from git import Repo
+
+mcp=FastMCP(name="RepoRadar")
+
+CLONE_DIR = "cloned_repos"
 
 
-if __name__ == "__main__":
-    main()
+@mcp.tool
+def clone_repo(url: str) -> str:
+    repo_name = url.split("/")[-1].replace(".git", "")
+    path = os.path.join(CLONE_DIR, repo_name)
+    if not os.path.exists(path):
+        Repo.clone_from(url, path)
+    return path
+
+
+
+
+@mcp.tool
+def generate_tree(start_path, prefix=""):
+    """
+    this is a helper function which returns the tree structure of the repository
+    """
+
+    items = sorted(os.listdir(start_path))
+    for index, item in enumerate(items):
+        path = os.path.join(start_path, item)
+        connector = "└── " if index == len(items) - 1 else "├── "
+        print(prefix + connector + item)
+        if os.path.isdir(path):
+            extension = "    " if index == len(items) - 1 else "│   "
+            generate_tree(path, prefix + extension)
+
+
+
+
